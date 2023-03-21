e02d78b9f4cc911663b9de16f2145a6fc58254c0
farsi mac <farisaizy12@gmail.com>
2023-03-21 13:20:06 +0700

    update bug

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -33,7 +33,7 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image" style="background-image: url({{URL::asset('assets/img/20220807_175847.png') }});background-size: contain;"></div>
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image" style="background-image: url({{URL::asset('assets/img/20220807_175847.png') }});"></div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
